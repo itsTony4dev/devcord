28543811c86a2ceb92358c077ad5c2d8ef9d432b
itsTony4dev <tonyayda7@gmail.com>
2025-01-17 21:53:11 +0200

    get conversation

--- backend/routes/chat.routes.js
+++ backend/routes/chat.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { sendMessage } from "../controller/chat.controller.js";
+import { getConversation, sendMessage } from "../controller/chat.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.route("/send/:id").post(protectRoute, sendMessage);
+router.route("/:id").get(protectRoute, getConversation);
 
 export default router;
